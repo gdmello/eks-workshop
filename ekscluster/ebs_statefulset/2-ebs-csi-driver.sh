84ed8ebab958b321c82f406d72877093eea0f049
gdmello <gavin.dmello@points.com>
2023-01-27 17:22:41 -0500

    updates to support latest version of chart

--- ekscluster/ebs_statefulset/2-ebs-csi-driver.sh
+++ ekscluster/ebs_statefulset/2-ebs-csi-driver.sh
@@ -7,13 +7,11 @@ helm search  repo aws-ebs-csi-driver
 helm upgrade --install aws-ebs-csi-driver \
   --version=2.16.0 \
   --namespace kube-system \
-  --set serviceAccount.controller.create=false \
-  --set serviceAccount.snapshot.create=false \
+  --set controller.serviceAccount.create=false \
   --set enableVolumeScheduling=true \
   --set enableVolumeResizing=true \
   --set enableVolumeSnapshot=true \
-  --set serviceAccount.snapshot.name=ebs-csi-controller-irsa \
-  --set serviceAccount.controller.name=ebs-csi-controller-irsa \
+  --set controller.serviceAccount.name=ebs-csi-controller-irsa \
   aws-ebs-csi-driver/aws-ebs-csi-driver
 
   kubectl -n kube-system rollout status deployment ebs-csi-controller
